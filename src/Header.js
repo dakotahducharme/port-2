bff98b2b90fbad2a6a81b3f3f21c8da10e76ae12
Dakotah Ducharme <dakotahbducharme@gmail.com>
2019-08-06 19:10:53 -0700

    Navbar

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import styled from "styled-components";
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-class Header extends Component {
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
   render() {
     return (
-      <p>hi</p>
-    )
+      <div>
+        <Navbar color="faded" light>
+          <NavbarBrand href="/" className="mr-auto">dakotah ducharme</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/">Past work</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/dakotahducharme">GitHub</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
   }
 }
-
-export default Header;
